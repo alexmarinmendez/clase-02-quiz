6a1ed13272f457a755b5e3bdfc5a140c2fa6250a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-08-11 21:00:44 -0500

    feat: rendering the next question

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ import { questionsArray } from "./questions.js"
 import { Quiz } from "./models/Quiz.js"
 import { UI } from './models/UI.js'
 
-const main = () => {
-    const quiz = new Quiz(questionsArray)
-    const ui = new UI()
-
+const renderPage = (quiz, ui) => {
     ui.showQuestion(quiz.getCurrentQuestion().text)
     ui.showChoices(quiz.getCurrentQuestion().choices, (currentChoice) => {
         quiz.validateAndContinue(currentChoice)
         console.log(quiz.score)
+        renderPage(quiz, ui)
     })
+}
+
+const main = () => {
+    const quiz = new Quiz(questionsArray)
+    const ui = new UI()
+
+    renderPage(quiz, ui)
 }
 
 main()

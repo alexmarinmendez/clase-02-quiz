dbfb9bc0e2faf9d7e6c85bd6786808b7ffaec3e9
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-08-11 20:13:50 -0500

    feat: data mapping

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { data } from './data.js'
 import { Question } from './models/Question.js'
 
-const question = new Question(data[0].question, data[0].choices, data[0].answer)
-const result = question.isCorrectAnswer('High Level Languages')
+const questionsArray = data.map(q => new Question(q.question, q.choices, q.answer))
+
+// const question = new Question(data[0].question, data[0].choices, data[0].answer)
+const result = questionsArray[0].isCorrectAnswer('High Level Languages')
 console.log(result)

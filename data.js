e7e414092a57176ff6091ec92e2c198dd950e2b0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-08-11 19:54:57 -0500

    feat: initial boilerplate

---
+++ data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+      question: "What is the only thing that computers understand?",
+      choices: ["Machine Code", "High Level Languages", "Low Level Languages", "Algorithms"],
+      answer: "Machine Code",
+    },
+    {
+      question: "A list of instructions that enable a computer to perform a specific task is a...",
+      choices: ["Computer Program", "Machine Code", "Algorithm", "Binary Code"],
+      answer: "Algorithm",
+    },
+    {
+      question: "Before a computer can understand a program it must be...",
+      choices: ["Translated into its machine code", "Translated into a low level language", "Translated into a high level language"],
+      answer: "Translated into its machine code",
+    },
+  ];
